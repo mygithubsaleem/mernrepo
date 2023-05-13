ee8fa0d83eae39d150d6183101e2887b2e621299
mygithubsaleem <113419103+mygithubsaleem@users.noreply.github.com>
2023-05-13 22:54:26 +0500

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../App.css";
+
+const Home = () => {
+	return (
+		<>
+			<div className="home-page">
+				<div className="home-page-contents">
+					<h1>WELCOME HOME</h1>
+					<h2>We are MERN developers...</h2>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Home;
